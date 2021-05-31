980107c7a10514ad5ef3874f9a8c092830ed0335
Douglas Cristhian <63408725+douglasrocha06@users.noreply.github.com>
2021-05-31 10:45:11 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = '12345678'
 app.config['MYSQL_DATABASE_DB'] = 'api_clientes'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = 'djlbanco.cxycaymkd24m.us-east-1.rds.amazonaws.com'
 mysql.init_app(app)
 
 mysql2 = MySQL()
-app2.config['MYSQL_DATABASE_USER'] = 'root'
-app2.config['MYSQL_DATABASE_PASSWORD'] = ''
+app2.config['MYSQL_DATABASE_USER'] = 'admin'
+app2.config['MYSQL_DATABASE_PASSWORD'] = '12345678'
 app2.config['MYSQL_DATABASE_DB'] = 'api_produtos'
-app2.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app2.config['MYSQL_DATABASE_HOST'] = 'djlbanco.cxycaymkd24m.us-east-1.rds.amazonaws.com'
 mysql2.init_app(app2)
 
 mysql3 = MySQL()
-app3.config['MYSQL_DATABASE_USER'] = 'root'
-app3.config['MYSQL_DATABASE_PASSWORD'] = ''
+app3.config['MYSQL_DATABASE_USER'] = 'admin'
+app3.config['MYSQL_DATABASE_PASSWORD'] = '12345678'
 app3.config['MYSQL_DATABASE_DB'] = 'api_inventario'
-app3.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app3.config['MYSQL_DATABASE_HOST'] = 'djlbanco.cxycaymkd24m.us-east-1.rds.amazonaws.com'
 mysql3.init_app(app3)
