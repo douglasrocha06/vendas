bd924bac157c3f62d7995b3f72d9b390c0e16986
Douglas Cristhian <63408725+douglasrocha06@users.noreply.github.com>
2021-06-02 15:35:01 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ from flaskext.mysql import MySQL
 mysql3 = MySQL()
 app3.config['MYSQL_DATABASE_USER'] = 'admin'
 app3.config['MYSQL_DATABASE_PASSWORD'] = '12345678'
+app3.config['MYSQL_DATABASE_DB'] = 'api_clientes'
 app3.config['MYSQL_DATABASE_DB'] = 'api_inventario'
+app3.config['MYSQL_DATABASE_DB'] = 'api_produtos'
 app3.config['MYSQL_DATABASE_HOST'] = 'djlbanco.cxycaymkd24m.us-east-1.rds.amazonaws.com'
 mysql3.init_app(app3)
